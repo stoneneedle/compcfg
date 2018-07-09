895847c52bce15387ad81b9eeab5bdf9cafbf145
stoneneedle <stoneneedle@gmail.com>
2018-07-09 12:57:11 +0800

    added sublime 3

--- jimbocfg.sh
+++ jimbocfg.sh
@@ -35,5 +35,10 @@ else
 	# Dropbox
 
 	# Sublime 3
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+	sudo apt-get install apt-transport-https
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+	sudo apt-get update
+	sudo apt-get install sublime-text
 fi
 exit 0
